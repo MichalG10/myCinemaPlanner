3712567a9041bef9aba56593ff42506138d0fdc5
DKaczmarek <magiczny.dominik.k@gmail.com>
2017-12-12 19:24:46 +0100

    update - added sqlscripts

---
+++ sqlscripts/add_views.sql
@@ -0,0 +1,5 @@
+﻿create view Distributors_counted_movies as
+select d.Name, count(dis.DistributionID) as AmountofMovies
+from Distributors as d
+left join Distributions dis on dis.DistributorID = d.DistributorID
+group by d.Name
